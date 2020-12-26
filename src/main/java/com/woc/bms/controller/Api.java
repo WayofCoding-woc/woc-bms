a3602e2d60e0173d705b20619af75d1f3dae96f7
woc <wayofcoding@gmail.com>
2020-12-26 09:53:37 +0530

    added logger message to print theatre object for debugging purpose

--- src/main/java/com/woc/bms/controller/Api.java
+++ src/main/java/com/woc/bms/controller/Api.java
@@ -4,6 +4,8 @@ import com.woc.bms.data.model.City;
 import com.woc.bms.data.model.Theatre;
 import com.woc.bms.service.DataService;
 import java.util.List;
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @RequestMapping("/api")
 public class Api {
@@ -28,6 +31,8 @@ public class Api {
     public List<Theatre> getTheatreByCityCode(@PathVariable("cityCode") String cityCode){
         System.out.println("getTheatreByCityCode api for cityCode="+cityCode + " has been triggered");
         List<Theatre> theatres = dataService.getTheatreByCityCode(cityCode);
+
+        log.debug(theatres);
         return theatres;
     }
 
